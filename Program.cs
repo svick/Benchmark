3dd3371f1ca78d5630960b8708965f0a38612afd
Petr Onderka <gsvick@gmail.com>
2018-02-23 20:40:29 +0100

    Prepare for Linux

--- Program.cs
+++ Program.cs
@@ -17,52 +17,55 @@ public class Program
     static void Main() => BenchmarkRunner.Run<Program>();
     //static void Main() => new Program().Benchmark();
 
-    List<string> files = Directory.GetFiles(@"C:\code\roslyn\src\Compilers\CSharp\Portable", "*.cs", SearchOption.AllDirectories)
-        .Append(@"C:\code\roslyn\src\Compilers\CSharp\CSharpAnalyzerDriver\CSharpDeclarationComputer.cs")
+    const string RoslynDir = @"C:\code\roslyn";
+    static readonly string UserDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+    List<string> files = Directory.GetFiles($@"{RoslynDir}\src\Compilers\CSharp\Portable", "*.cs", SearchOption.AllDirectories)
+        .Append($@"{RoslynDir}\src\Compilers\CSharp\CSharpAnalyzerDriver\CSharpDeclarationComputer.cs")
         .ToList();
         
     List<MetadataReference> references = new[] 
     {
-        @"C:\code\roslyn\Binaries\Debug\Dlls\CodeAnalysis\ref\Microsoft.CodeAnalysis.dll",
-        @"C:\Users\Svick\.nuget\packages\system.collections.concurrent\4.3.0\ref\netstandard1.3\System.Collections.Concurrent.dll",
-        @"C:\Users\Svick\.nuget\packages\system.collections\4.3.0\ref\netstandard1.3\System.Collections.dll",
-        @"C:\Users\Svick\.nuget\packages\system.collections.immutable\1.3.1\lib\netstandard1.0\System.Collections.Immutable.dll",
-        @"C:\Users\Svick\.nuget\packages\system.diagnostics.debug\4.3.0\ref\netstandard1.3\System.Diagnostics.Debug.dll",
-        @"C:\Users\Svick\.nuget\packages\system.diagnostics.fileversioninfo\4.3.0\ref\netstandard1.3\System.Diagnostics.FileVersionInfo.dll",
-        @"C:\Users\Svick\.nuget\packages\system.diagnostics.tools\4.3.0\ref\netstandard1.0\System.Diagnostics.Tools.dll",
-        @"C:\Users\Svick\.nuget\packages\system.globalization\4.3.0\ref\netstandard1.3\System.Globalization.dll",
-        @"C:\Users\Svick\.nuget\packages\system.io.compression\4.3.0\ref\netstandard1.3\System.IO.Compression.dll",
-        @"C:\Users\Svick\.nuget\packages\system.io\4.3.0\ref\netstandard1.3\System.IO.dll",
-        @"C:\Users\Svick\.nuget\packages\system.io.filesystem\4.3.0\ref\netstandard1.3\System.IO.FileSystem.dll",
-        @"C:\Users\Svick\.nuget\packages\system.io.filesystem.primitives\4.3.0\ref\netstandard1.3\System.IO.FileSystem.Primitives.dll",
-        @"C:\Users\Svick\.nuget\packages\system.linq\4.3.0\ref\netstandard1.0\System.Linq.dll",
-        @"C:\Users\Svick\.nuget\packages\system.linq.expressions\4.3.0\ref\netstandard1.3\System.Linq.Expressions.dll",
-        @"C:\Users\Svick\.nuget\packages\system.reflection\4.3.0\ref\netstandard1.3\System.Reflection.dll",
-        @"C:\Users\Svick\.nuget\packages\system.reflection.extensions\4.3.0\ref\netstandard1.0\System.Reflection.Extensions.dll",
-        @"C:\Users\Svick\.nuget\packages\system.reflection.metadata\1.4.2\lib\netstandard1.1\System.Reflection.Metadata.dll",
-        @"C:\Users\Svick\.nuget\packages\system.reflection.primitives\4.3.0\ref\netstandard1.0\System.Reflection.Primitives.dll",
-        @"C:\Users\Svick\.nuget\packages\system.resources.resourcemanager\4.3.0\ref\netstandard1.0\System.Resources.ResourceManager.dll",
-        @"C:\Users\Svick\.nuget\packages\system.runtime\4.3.0\ref\netstandard1.3\System.Runtime.dll",
-        @"C:\Users\Svick\.nuget\packages\system.runtime.extensions\4.3.0\ref\netstandard1.3\System.Runtime.Extensions.dll",
-        @"C:\Users\Svick\.nuget\packages\system.runtime.handles\4.3.0\ref\netstandard1.3\System.Runtime.Handles.dll",
-        @"C:\Users\Svick\.nuget\packages\system.runtime.interopservices\4.3.0\ref\netstandard1.3\System.Runtime.InteropServices.dll",
-        @"C:\Users\Svick\.nuget\packages\system.runtime.numerics\4.3.0\ref\netstandard1.1\System.Runtime.Numerics.dll",
-        @"C:\Users\Svick\.nuget\packages\system.security.cryptography.algorithms\4.3.0\ref\netstandard1.3\System.Security.Cryptography.Algorithms.dll",
-        @"C:\Users\Svick\.nuget\packages\system.security.cryptography.encoding\4.3.0\ref\netstandard1.3\System.Security.Cryptography.Encoding.dll",
-        @"C:\Users\Svick\.nuget\packages\system.security.cryptography.primitives\4.3.0\ref\netstandard1.3\System.Security.Cryptography.Primitives.dll",
-        @"C:\Users\Svick\.nuget\packages\system.security.cryptography.x509certificates\4.3.0\ref\netstandard1.3\System.Security.Cryptography.X509Certificates.dll",
-        @"C:\Users\Svick\.nuget\packages\system.text.encoding.codepages\4.3.0\ref\netstandard1.3\System.Text.Encoding.CodePages.dll",
-        @"C:\Users\Svick\.nuget\packages\system.text.encoding\4.3.0\ref\netstandard1.3\System.Text.Encoding.dll",
-        @"C:\Users\Svick\.nuget\packages\system.text.encoding.extensions\4.3.0\ref\netstandard1.3\System.Text.Encoding.Extensions.dll",
-        @"C:\Users\Svick\.nuget\packages\system.threading\4.3.0\ref\netstandard1.3\System.Threading.dll",
-        @"C:\Users\Svick\.nuget\packages\system.threading.tasks\4.3.0\ref\netstandard1.3\System.Threading.Tasks.dll",
-        @"C:\Users\Svick\.nuget\packages\system.threading.tasks.parallel\4.3.0\ref\netstandard1.1\System.Threading.Tasks.Parallel.dll",
-        @"C:\Users\Svick\.nuget\packages\system.valuetuple\4.3.0\lib\netstandard1.0\System.ValueTuple.dll",
-        @"C:\Users\Svick\.nuget\packages\system.xml.readerwriter\4.3.0\ref\netstandard1.3\System.Xml.ReaderWriter.dll",
-        @"C:\Users\Svick\.nuget\packages\system.xml.xdocument\4.3.0\ref\netstandard1.3\System.Xml.XDocument.dll",
-        @"C:\Users\Svick\.nuget\packages\system.xml.xmldocument\4.3.0\ref\netstandard1.3\System.Xml.XmlDocument.dll",
-        @"C:\Users\Svick\.nuget\packages\system.xml.xpath\4.3.0\ref\netstandard1.3\System.Xml.XPath.dll",
-        @"C:\Users\Svick\.nuget\packages\system.xml.xpath.xdocument\4.3.0\ref\netstandard1.3\System.Xml.XPath.XDocument.dll"
+        $@"{RoslynDir}\Binaries\Debug\Dlls\CodeAnalysis\ref\Microsoft.CodeAnalysis.dll",
+        $@"{UserDir}\.nuget\packages\system.collections.concurrent\4.3.0\ref\netstandard1.3\System.Collections.Concurrent.dll",
+        $@"{UserDir}\.nuget\packages\system.collections\4.3.0\ref\netstandard1.3\System.Collections.dll",
+        $@"{UserDir}\.nuget\packages\system.collections.immutable\1.3.1\lib\netstandard1.0\System.Collections.Immutable.dll",
+        $@"{UserDir}\.nuget\packages\system.diagnostics.debug\4.3.0\ref\netstandard1.3\System.Diagnostics.Debug.dll",
+        $@"{UserDir}\.nuget\packages\system.diagnostics.fileversioninfo\4.3.0\ref\netstandard1.3\System.Diagnostics.FileVersionInfo.dll",
+        $@"{UserDir}\.nuget\packages\system.diagnostics.tools\4.3.0\ref\netstandard1.0\System.Diagnostics.Tools.dll",
+        $@"{UserDir}\.nuget\packages\system.globalization\4.3.0\ref\netstandard1.3\System.Globalization.dll",
+        $@"{UserDir}\.nuget\packages\system.io.compression\4.3.0\ref\netstandard1.3\System.IO.Compression.dll",
+        $@"{UserDir}\.nuget\packages\system.io\4.3.0\ref\netstandard1.3\System.IO.dll",
+        $@"{UserDir}\.nuget\packages\system.io.filesystem\4.3.0\ref\netstandard1.3\System.IO.FileSystem.dll",
+        $@"{UserDir}\.nuget\packages\system.io.filesystem.primitives\4.3.0\ref\netstandard1.3\System.IO.FileSystem.Primitives.dll",
+        $@"{UserDir}\.nuget\packages\system.linq\4.3.0\ref\netstandard1.0\System.Linq.dll",
+        $@"{UserDir}\.nuget\packages\system.linq.expressions\4.3.0\ref\netstandard1.3\System.Linq.Expressions.dll",
+        $@"{UserDir}\.nuget\packages\system.reflection\4.3.0\ref\netstandard1.3\System.Reflection.dll",
+        $@"{UserDir}\.nuget\packages\system.reflection.extensions\4.3.0\ref\netstandard1.0\System.Reflection.Extensions.dll",
+        $@"{UserDir}\.nuget\packages\system.reflection.metadata\1.4.2\lib\netstandard1.1\System.Reflection.Metadata.dll",
+        $@"{UserDir}\.nuget\packages\system.reflection.primitives\4.3.0\ref\netstandard1.0\System.Reflection.Primitives.dll",
+        $@"{UserDir}\.nuget\packages\system.resources.resourcemanager\4.3.0\ref\netstandard1.0\System.Resources.ResourceManager.dll",
+        $@"{UserDir}\.nuget\packages\system.runtime\4.3.0\ref\netstandard1.3\System.Runtime.dll",
+        $@"{UserDir}\.nuget\packages\system.runtime.extensions\4.3.0\ref\netstandard1.3\System.Runtime.Extensions.dll",
+        $@"{UserDir}\.nuget\packages\system.runtime.handles\4.3.0\ref\netstandard1.3\System.Runtime.Handles.dll",
+        $@"{UserDir}\.nuget\packages\system.runtime.interopservices\4.3.0\ref\netstandard1.3\System.Runtime.InteropServices.dll",
+        $@"{UserDir}\.nuget\packages\system.runtime.numerics\4.3.0\ref\netstandard1.1\System.Runtime.Numerics.dll",
+        $@"{UserDir}\.nuget\packages\system.security.cryptography.algorithms\4.3.0\ref\netstandard1.3\System.Security.Cryptography.Algorithms.dll",
+        $@"{UserDir}\.nuget\packages\system.security.cryptography.encoding\4.3.0\ref\netstandard1.3\System.Security.Cryptography.Encoding.dll",
+        $@"{UserDir}\.nuget\packages\system.security.cryptography.primitives\4.3.0\ref\netstandard1.3\System.Security.Cryptography.Primitives.dll",
+        $@"{UserDir}\.nuget\packages\system.security.cryptography.x509certificates\4.3.0\ref\netstandard1.3\System.Security.Cryptography.X509Certificates.dll",
+        $@"{UserDir}\.nuget\packages\system.text.encoding.codepages\4.3.0\ref\netstandard1.3\System.Text.Encoding.CodePages.dll",
+        $@"{UserDir}\.nuget\packages\system.text.encoding\4.3.0\ref\netstandard1.3\System.Text.Encoding.dll",
+        $@"{UserDir}\.nuget\packages\system.text.encoding.extensions\4.3.0\ref\netstandard1.3\System.Text.Encoding.Extensions.dll",
+        $@"{UserDir}\.nuget\packages\system.threading\4.3.0\ref\netstandard1.3\System.Threading.dll",
+        $@"{UserDir}\.nuget\packages\system.threading.tasks\4.3.0\ref\netstandard1.3\System.Threading.Tasks.dll",
+        $@"{UserDir}\.nuget\packages\system.threading.tasks.parallel\4.3.0\ref\netstandard1.1\System.Threading.Tasks.Parallel.dll",
+        $@"{UserDir}\.nuget\packages\system.valuetuple\4.3.0\lib\netstandard1.0\System.ValueTuple.dll",
+        $@"{UserDir}\.nuget\packages\system.xml.readerwriter\4.3.0\ref\netstandard1.3\System.Xml.ReaderWriter.dll",
+        $@"{UserDir}\.nuget\packages\system.xml.xdocument\4.3.0\ref\netstandard1.3\System.Xml.XDocument.dll",
+        $@"{UserDir}\.nuget\packages\system.xml.xmldocument\4.3.0\ref\netstandard1.3\System.Xml.XmlDocument.dll",
+        $@"{UserDir}\.nuget\packages\system.xml.xpath\4.3.0\ref\netstandard1.3\System.Xml.XPath.dll",
+        $@"{UserDir}\.nuget\packages\system.xml.xpath.xdocument\4.3.0\ref\netstandard1.3\System.Xml.XPath.XDocument.dll"
     }.Select(f => MetadataReference.CreateFromFile(f)).ToList<MetadataReference>();
 
     CSharpCompilation CreateCompilation(DocumentationMode documentationMode)
@@ -77,7 +80,7 @@ public class Program
 
         return CSharpCompilation.Create(
             "Microsoft.CodeAnalysis.CSharp", trees, references,
-            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, cryptoKeyFile: @"C:\code\roslyn\build\Targets\..\Strong Name Keys\35MSSharedLib1024.snk", publicSign: true));
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, cryptoKeyFile: $@"{RoslynDir}\build\Targets\..\Strong Name Keys\35MSSharedLib1024.snk", publicSign: true));
     }
 
     [Params(EmitMode.None, EmitMode.Null, EmitMode.NullStream)]
