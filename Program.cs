3504a3c53560613b014c7de1b31d31f6a582c99e
Petr Onderka <gsvick@gmail.com>
2018-03-18 00:49:35 +0100

    Just Add and indexer

--- Program.cs
+++ Program.cs
@@ -1,34 +1,25 @@
-﻿using System;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
+﻿using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
 using BenchmarkDotNet.Running;
 
 public class Program
 {
     static void Main() => BenchmarkRunner.Run<Bench>();
-    //static void Main() => new Bench().MyInlinedCachedBuilderIndexer();
+    //static void Main() => new Bench().Indexer();
 }
 
-[MemoryDiagnoser]
-[DisassemblyDiagnoser(printAsm: true, printSource: true)]
+[InProcess]
 public class Bench
 {
-    static readonly ThreadLocal<ImmutableArray<long>.Builder> _cachedBuilder = new ThreadLocal<ImmutableArray<long>.Builder>(ImmutableArray.CreateBuilder<long>);
-    static readonly ThreadLocal<MyImmutable.ImmutableArray<long>.Builder> _myCachedBuilder = new ThreadLocal<MyImmutable.ImmutableArray<long>.Builder>(MyImmutable.ImmutableArray.CreateBuilder<long>);
-    static readonly ThreadLocal<MyImmutableInlined.ImmutableArray<long>.Builder> _myInlinedCachedBuilder = new ThreadLocal<MyImmutableInlined.ImmutableArray<long>.Builder>(MyImmutableInlined.ImmutableArray.CreateBuilder<long>);
-    static readonly Func<long[], ImmutableArray<long>> _unsafeFreeze = GetUnsafeFreeze<long>();
-
     public const int N = 1000;
 
     [Benchmark]
-    public void CachedBuilder()
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void Add()
     {
-        var builder = _cachedBuilder.Value;
+        var builder = ImmutableArray.CreateBuilder<long>();
         builder.Capacity = 1000;
         for (long i = 0; i < builder.Capacity; i++)
         {
@@ -37,80 +28,15 @@ public class Bench
         builder.MoveToImmutable();
     }
     [Benchmark]
-    public void CachedBuilderIndexer()
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void Indexer()
     {
-        var builder = _cachedBuilder.Value;
+        var builder = ImmutableArray.CreateBuilder<long>();
         builder.Count = 1000;
         for (int i = 0; i < builder.Count; i++)
         {
             builder[i] = i;
         }
         builder.MoveToImmutable();
-    }
-    [Benchmark]
-    public void MyCachedBuilder()
-    {
-        var builder = _myCachedBuilder.Value;
-        builder.Capacity = N;
-        for (long i = 0; i < builder.Capacity; i++)
-        {
-            builder.Add(i);
-        }
-        builder.MoveToImmutable();
-    }
-    [Benchmark]
-    public void MyInlinedCachedBuilder()
-    {
-        var builder = _myInlinedCachedBuilder.Value;
-        builder.Capacity = N;
-        for (long i = 0; i < builder.Capacity; i++)
-        {
-            builder.Add(i);
-        }
-        builder.MoveToImmutable();
-    }
-    [Benchmark]
-    public void MyCachedBuilderIndexer()
-    {
-        var builder = _myCachedBuilder.Value;
-        builder.Count = 1000;
-        for (int i = 0; i < builder.Count; i++)
-        {
-            builder[i] = i;
-        }
-        builder.MoveToImmutable();
-    }
-    [Benchmark]
-    public void MyInlinedCachedBuilderIndexer()
-    {
-        var builder = _myInlinedCachedBuilder.Value;
-        builder.Count = 1000;
-        for (int i = 0; i < builder.Count; i++)
-        {
-            builder[i] = i;
-        }
-        builder.MoveToImmutable();
-    }
-
-    [Benchmark(Baseline = true)]
-    public void UnsafeFreeze()
-    {
-        var array = new long[N];
-        for (long i = 0; i < array.Length; i++)
-        {
-            array[i] = i;
-        }
-        _unsafeFreeze(array);
-    }
-
-    static Func<T[], ImmutableArray<T>> GetUnsafeFreeze<T>()
-    {
-        var ctor = typeof(ImmutableArray<T>)
-            .GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
-            .Single(c => c.GetParameters().Count() == 1 && c.GetParameters().Single().ParameterType.Equals(typeof(T[])));
-        var param = Expression.Parameter(typeof(T[]));
-        var body = Expression.New(ctor, param);
-        var func = Expression.Lambda<Func<T[], ImmutableArray<T>>>(body, param);
-        return func.Compile();
     }
 }
