8e8816534d1f8d2c50ba3d0a5f73ad131a02b0ba
Petr Onderka <gsvick@gmail.com>
2018-09-29 20:10:24 +0200

    Don't remember

--- Program.cs
+++ Program.cs
@@ -18,6 +18,9 @@ using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.CsProj;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 public class Program
 {
@@ -29,10 +32,10 @@ public class MainConfig : ManualConfig
 {
     public MainConfig()
     {
-        Add(Job.ShortRun.With(Platform.X64).With(CsProjClassicNetToolchain.Net471));
+        //Add(Job.ShortRun.With(Platform.X64).With(CsProjClassicNetToolchain.Net471));
         //Add(Job.Default.With(Platform.X64).With(CsProjCoreToolchain.NetCoreApp11));
         //Add(Job.Default.With(Platform.X64).With(CsProjCoreToolchain.NetCoreApp20));
-        Add(Job.ShortRun.With(Platform.X64).With(CsProjCoreToolchain.NetCoreApp21));
+        //Add(Job.ShortRun.With(Platform.X64).With(CsProjCoreToolchain.NetCoreApp21));
         //Add(MemoryDiagnoser.Default);
     }
 }
@@ -40,163 +43,66 @@ public class MainConfig : ManualConfig
 //[Config(typeof(MainConfig))]
 //[DisassemblyDiagnoser(printAsm: true, printSource: true)]
 //[ShortRunJob]
-[MemoryDiagnoser]
+//[MemoryDiagnoser]
 public class Benchmark
 {
-    [Params(1, 10/*, 100*/)]
-    public int N;
+    [Params(1, 2, 3, 4)]
+    public int Stage;
 
+    CSharpCompilation baseCompilation = CSharpCompilation.Create(null)
+        .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
 
     [Benchmark]
-    public async Task TaskDelay()
+    public void GetAttributes()
     {
-        for (int i = 0; i < N; i++)
+        string code = @"
+using System;
+
+[A]
+class Foo {}
+
+[A]
+class Bar {}
+
+class A : Attribute {}
+";
+
+        var tree = SyntaxFactory.ParseSyntaxTree(code);
+
+        var classes = tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
+
+        foreach (var c in classes)
         {
-            await Task.Delay(TimeSpan.FromMilliseconds(1));
-        }
-    }
-
-    /*[Benchmark]
-    public async Task DelayTaskSource()
-    {
-        var delayTaskSource = new DelayTaskSource();
-
-        for (int i = 0; i < N; i++)
-        {
-            await delayTaskSource.Delay(TimeSpan.FromMilliseconds(1));
-        }
-    }*/
-}
-
-class DelayTaskSource
-{
-    readonly ValueTaskSource valueTaskSource;
-    readonly object l = new object();
-    readonly List<(Action<object>, object, short)> continuations = new List<(Action<object>, object, short)>();
-    readonly Dictionary<short, long> timestamps = new Dictionary<short, long>();
-    readonly List<short> tokenCache = new List<short>();
-    readonly Timer timer;
-    long? timerTimestamp;
-    short nextToken;
-
-    public DelayTaskSource()
-    {
-        valueTaskSource = new ValueTaskSource(this);
-        timer = new Timer(TimerCallback);
-    }
-
-    private static long ToTicks(TimeSpan timeSpan) => (long)(timeSpan.TotalSeconds * Stopwatch.Frequency);
-    private static TimeSpan ToTimeSpan(long ticks) => TimeSpan.FromSeconds((double)ticks / Stopwatch.Frequency);
-
-    public ValueTask Delay(TimeSpan delay)
-    {
-        long nowTimestamp = Stopwatch.GetTimestamp();
-        long delayTimestamp = nowTimestamp + ToTicks(delay);
-
-        lock (l)
-        {
-            if (nextToken < 0)
-                throw new Exception("token overflow");
-
-            short token = nextToken++;
-
-            timestamps.Add(token, delayTimestamp);
-
-            if (timerTimestamp == null || timerTimestamp > delayTimestamp)
-            {
-                timer.Change(delay, Timeout.InfiniteTimeSpan);
-                timerTimestamp = delayTimestamp;
-            }
-
-            return new ValueTask(valueTaskSource, token);
-        }
-    }
-
-    private void TimerCallback(object _)
-    {
-        long nowTimestamp = Stopwatch.GetTimestamp();
-        Action<object> continuation = null;
-        object state = null;
-
-        long? nextTimestamp = null;
-
-        lock (l)
-        {
-            tokenCache.Clear();
-
-            foreach (var (token, timestamp) in timestamps)
-            {
-                if (timestamp <= nowTimestamp) 
-                {
-                    tokenCache.Add(token);
-                }
-                else if (nextTimestamp == null || timestamp < nextTimestamp)
-                {
-                    nextTimestamp = timestamp;
-                }
-            }
-
-            if (nextTimestamp != null)
-                timer.Change(ToTimeSpan(nextTimestamp.Value - nowTimestamp), Timeout.InfiniteTimeSpan);
-            timerTimestamp = nextTimestamp;
-
-            foreach (var token in tokenCache)
-            {
-                timestamps.Remove(token);
-            }
-
-            foreach (var (c, s, token) in continuations)
-            {
-                if (!timestamps.ContainsKey(token))
-                {
-                    continuation = c;
-                    state = s;
-                }
-            }
+            foreach (var a in c.AttributeLists.Select(al => al.Attributes))
+            { }
         }
 
-        continuation?.Invoke(state);
-    }
+        if (Stage == 0) return;
 
-    class ValueTaskSource : IValueTaskSource
-    {
-        readonly DelayTaskSource parent;
+        var compilation = baseCompilation.AddSyntaxTrees(tree);
 
-        public ValueTaskSource(DelayTaskSource parent) => this.parent = parent;
+        if (Stage == 1) return;
 
-        public void GetResult(short token)
-        {
-            lock (parent.l)
-            {
-                if (parent.timestamps.ContainsKey(token))
-                    throw new InvalidOperationException("not yet completed");
-            }
-        }
+        var model = compilation.GetSemanticModel(tree);
 
-        public ValueTaskSourceStatus GetStatus(short token)
-        {
-            lock (parent.l)
-            {
-                if (parent.timestamps.ContainsKey(token))
-                    return ValueTaskSourceStatus.Pending;
+        if (Stage == 2) return;
 
-                return ValueTaskSourceStatus.Succeeded;
-            }
-        }
+        var fooClass = classes[0];
 
-        public void OnCompleted(Action<object> continuation, object state, short token, ValueTaskSourceOnCompletedFlags flags)
-        {
-            lock (parent.l)
-            {
-                if (parent.timestamps.ContainsKey(token))
-                {
-                    parent.continuations.Add((continuation, state, token));
-                    return;
-                }
-            }
+        var fooAttribute = fooClass.AttributeLists.Single().Attributes.Single();
 
-            // already completed, invoke inline
-            continuation(state);
-        }
+        var fooSymbol = model.GetSymbolInfo(fooAttribute).Symbol;
+
+        if (fooSymbol == null) throw new Exception();
+
+        if (Stage == 3) return;
+
+        var barClass = classes[1];
+
+        var barAttribute = barClass.AttributeLists.Single().Attributes.Single();
+
+        var barSymbol = model.GetSymbolInfo(barAttribute).Symbol;
+
+        if (barSymbol == null) throw new Exception();
     }
 }
